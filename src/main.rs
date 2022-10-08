e9c435ccd108c30bb70efd0e03787658fe59cbca
Nicolas Girot <ngirot@hotmail.com>
2022-10-08 15:26:52 +0200

    Clean code

--- src/main.rs
+++ src/main.rs
@@ -6,41 +6,52 @@ use sdl2::keyboard::Keycode;
 use sdl2::render::WindowCanvas;
 use std::time::Duration;
 
-fn render(canvas: &mut WindowCanvas) {
+fn render_clear(canvas: &mut WindowCanvas) {
     canvas.set_draw_color(Color::RGB(0, 0, 0));
     canvas.clear();
+}
 
+fn render_background(canvas: &mut WindowCanvas, width: u32, height: u32) {
     canvas.set_draw_color(Color::RGB(50, 50, 50));
-    canvas.fill_rect(Rect::new(0, 0, 800, 250))
-        .expect("Cannot render background");
+    let mid_screen = height/2;
+    canvas.fill_rect(Rect::new(0, 0, width, mid_screen))
+        .expect("Cannot render ceiling");
         
     canvas.set_draw_color(Color::RGB(100, 100, 100));
-    canvas.fill_rect(Rect::new(0, 251, 800, 500))
-        .expect("Cannot render background");
+    canvas.fill_rect(Rect::new(0, (mid_screen+1).try_into().unwrap(), width, height))
+        .expect("Cannot render floor");
+}
 
+fn render_walls(canvas: &mut WindowCanvas, width: i32, height: i32) {
 
-    for i in 0..800 {
+    for i in 0..width {
         let distance = if i < 300 {3} else if i>600 {2} else {1};
         canvas.set_draw_color(Color::RGB(0, 0, 255/(distance)));
 
-        let screen_length = 500;
         let biais: i32 = (distance*75).into();
         let start = Point::new(i, biais);
-        let end = Point::new(i, screen_length-biais);
+        let end = Point::new(i, height-biais);
 
         canvas.draw_line(start, end).expect("cannot render");
-
-        
     }
+}
 
+fn render_ask_display(canvas: &mut WindowCanvas) {
     canvas.present();
+}
+
+fn render(canvas: &mut WindowCanvas) {
+    render_clear(canvas);
+    render_background(canvas, 800, 500);
+    render_walls(canvas, 800, 500);
+    render_ask_display(canvas);
 }
 
 fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
 
-    let window = video_subsystem.window("wolfy engine", 800, 500)
+    let window = video_subsystem.window("Wolfengate engine", 800, 500)
         .position_centered()
         .build()
         .expect("could not initialize video subsystem");
